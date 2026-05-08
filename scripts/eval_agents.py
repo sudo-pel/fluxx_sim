92abe7acebb0b0abc492fd23b74ade10143e8ce5
Pelumi <pelumi@babalola.com>
2026-05-08 21:10:23 +0100

    add repeat matchup avoidance to eval_agents

--- scripts/eval_agents.py
+++ scripts/eval_agents.py
@@ -104,7 +104,7 @@ for card_list in args.card_lists:
     for agent_name in agent_names:
         for other_agent_name in agent_names:
             if agent_name == other_agent_name or (other_agent_name, agent_name) in seen_matchups: continue
-            seen_matchups.add((other_agent_name, agent_name))
+            seen_matchups.add((agent_name, other_agent_name))
 
             agent = agents[agent_name]
             other_agent = agents[other_agent_name]
