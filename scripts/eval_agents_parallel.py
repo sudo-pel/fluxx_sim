fb822e1d934017ce662bfb82523eeb41b2e7d2a9
Pelumi <pelumi@babalola.com>
2026-05-11 23:03:32 +0100

    typo

--- scripts/eval_agents_parallel.py
+++ scripts/eval_agents_parallel.py
@@ -34,7 +34,7 @@ CARD_LISTS = {
 AGENT_NAMES = [
     "ppo",
     "ppo_general",
-    "ppo_general_with_reward_shaping"
+    "ppo_general_with_reward_shaping",
     "dqn",
     "random",
     "heuristic_agent_mki",
