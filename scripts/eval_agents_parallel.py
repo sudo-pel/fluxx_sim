4a21c8546107ba20e44c24795be6d106e13a0ed9
Pelumi <pelumi@babalola.com>
2026-05-11 17:55:10 +0100

    attempt to fix cuda oom

--- scripts/eval_agents_parallel.py
+++ scripts/eval_agents_parallel.py
@@ -3,6 +3,7 @@ import os
 os.environ["OPENBLAS_NUM_THREADS"] = "1"
 os.environ["MKL_NUM_THREADS"] = "1"
 os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 from pathlib import Path
 
