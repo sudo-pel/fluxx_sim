cbb88c69d043c631ce4abbac866ce100387cf400
Pelumi <pelumi@babalola.com>
2026-05-11 17:45:19 +0100

    limit thread creation

--- scripts/eval_agents_parallel.py
+++ scripts/eval_agents_parallel.py
@@ -1,5 +1,9 @@
 import argparse
 import os
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["OMP_NUM_THREADS"] = "1"
+
 from pathlib import Path
 
 import torch
